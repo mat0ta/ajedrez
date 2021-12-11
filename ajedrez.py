2236bd2c56d6de311ee48b8a18869df6ae6631bf
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-11 18:22:22 +0100

    Creamos un obj que contenga todas las fichas y mediante un print obtenemos sus valores ASCII (los cuales se guardan comentados al lado de cada una de las fichas para más adelante)

--- ajedrez.py
+++ ajedrez.py
@@ -1,3 +1,33 @@
 # Importamos los módulos requeridos para realizar la actividad.
 from os import *
 from time import *
+
+# Creamos las variables necesarias.
+fichasAjedrez = {
+    ord('♔'): {  # 9812
+        'value': 5
+    }, ord('♕'): {  # 9813
+        'value': 5
+    }, ord('♖'): {  # 9814
+        'value': 5
+    }, ord('♗'): {  # 9815
+        'value': 5
+    }, ord('♘'): {  # 9816
+        'value': 5
+    }, ord('♙'): {  # 9817
+        'value': 5
+    }, ord('♚'): {  # 9818
+        'value': 5
+    }, ord('♛'): {  # 9819
+        'value': 5
+    }, ord('♜'): {  # 9820
+        'value': 5
+    }, ord('♝'): {  # 9821
+        'value': 5
+    }, ord('♞'): {  # 9822
+        'value': 5
+    }, ord('♟'): {  # 9823
+        'value': 5
+    }
+}
+print(fichasAjedrez[0].keys())
