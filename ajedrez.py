1cdad00c240840da8744b1eac8bf59aaab0f787e
Matota <72810000+mat0ta@users.noreply.github.com>
2021-12-11 18:24:59 +0100

    Update ajedrez.py

--- ajedrez.py
+++ ajedrez.py
@@ -5,29 +5,41 @@ from time import *
 # Creamos las variables necesarias.
 fichasAjedrez = {
     ord('♔'): {  # 9812
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♕'): {  # 9813
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♖'): {  # 9814
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♗'): {  # 9815
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♘'): {  # 9816
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♙'): {  # 9817
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♚'): {  # 9818
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♛'): {  # 9819
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♜'): {  # 9820
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♝'): {  # 9821
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♞'): {  # 9822
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }, ord('♟'): {  # 9823
-        'value': 5
+        'pos1': 5,
+        'pos2': 5
     }
 }
 print(fichasAjedrez[0].keys())
